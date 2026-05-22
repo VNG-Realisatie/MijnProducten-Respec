377d68d03e90502b5f681d460c5eabad806c4c07
RonaldJobse <ronald.jobse@vng.nl>
2026-05-22 15:49:46 +0200

    v0.2.0 - gebaseerd op OpenProduct en OpenProductType 1.6.0

--- js/config.js
+++ js/config.js
@@ -10,8 +10,8 @@ let respecConfig = {
   
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
   specStatus: "IO",
-  publishDate: "2026-04-29",
-  publishVersion: "0.1.0",
+  publishDate: "2026-05-22",
+  publishVersion: "0.2.0",
 
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane maturities
   //previousMaturity: "IO",
